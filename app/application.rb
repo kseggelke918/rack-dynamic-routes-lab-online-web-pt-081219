72b28282f8526b77ab7cf5b597cb96e64192c0c1
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-06 18:15:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,28 +19,7 @@ class Application
       resp.status = 404
       resp.write "Route not found"
     end 
-    
-    
-    
   resp.finish  
   end 
   
-  
-  
 end 
-
-
-
-# if req.path == "/items/"
-#       item = req.params["item"]
-#       if @@items.include?(item)
-#         resp.write "#{item.price}"
-#       else 
-#         resp.write "error message"
-#         resp.status = 400 
-#       end 
-#     else 
-#       resp.write "Route not found"
-#       resp.status = 404
-#     end 
-#     resp.finish  
