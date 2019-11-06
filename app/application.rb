0cfb7b19013e15ac790362ff8b2b1d8abbba4e73
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-06 18:13:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item.name = req.path.split("/items/").last 
-      item = @@items.find {|i| i.name == item.name}
+      item_name = req.path.split("/items/").last 
+      item = @@items.find {|i| i.name == item_name}
       resp.write "#{item.price}"
     else 
       resp.status = 404
