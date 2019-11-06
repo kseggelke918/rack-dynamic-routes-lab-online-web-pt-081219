ae7d5937ae44690a5d027526927d337f31ba2995
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-06 18:13:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/)
       item.name = req.path.split("/items/").last 
-      item = @@items.find {|i| i.price == item.price}
+      item = @@items.find {|i| i.name == item.name}
       resp.write "#{item.price}"
     else 
       resp.status = 404
