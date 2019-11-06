afe6d7fab5dd0fc20a4c6c0f57ce17fe903f7621
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-06 18:08:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path == "/items/"
+      item_name = req.path.split("/items/").last 
       item = req.params["item"]
       if @@item.include?(item)
         resp.write "#{item.price}"
