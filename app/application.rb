f3245006c951c909413d0a7eb4e0b5b151939731
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-06 18:14:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,11 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       item = @@items.find {|i| i.name == item_name}
-      resp.write "#{item.price}"
+      if item.exist?
+        resp.write "#{item.price}"
+      else 
+        resp.write "Item not found"
+        resp.status = 400 
     else 
       resp.status = 404
       resp.write "Route not found"
